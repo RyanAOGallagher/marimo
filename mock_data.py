30e58d149cd94773ccc044235b06f99451b1b0e6
Ryan <ryanaogallagher@gmail.com>
2023-11-06 14:05:23 +0900

    mock data script

---
+++ mock_data.py
@@ -0,0 +1,36 @@
+import serial
+import time
+import random
+
+# Replace 'COM3' with the serial port you want to use
+serial_port = 'COM3'
+baud_rate = 9600
+
+# Open the serial port
+ser = serial.Serial(serial_port, baud_rate)
+
+def generate_mock_data():
+    air_quality = random.choice(['Good', 'Bad', 'Normal', 'Worst'])
+    humidity = random.randint(20, 60) 
+    temperature = random.randint(15, 30) 
+    microphone_value = random.randint(100, 400)  
+
+    # Create the data string
+    data_str = f"Air Quality: {air_quality}  Humidity: {humidity}  Temperature: {temperature}  Microphone Value: {microphone_value}\n"
+    
+    return data_str
+
+try:
+    while True:
+        mock_data = generate_mock_data()
+        ser.write(mock_data.encode())
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("Stopped by User")
+
+except serial.SerialException as e:
+    print(f"Serial error: {e}")
+
+finally:
+    ser.close()
